0819d051e5509d1559f926f38845008d420c57e7
Shalu <jadaunshalu@gmail.com>
2019-08-26 18:36:46 -0700

    12

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+   {
+      return (
+         <div className="App">
+
+         <div class="header">
+            <h1>RESUME</h1>
+         </div>
+         <div class="section">
+            <div class="article">
+
+               <h1 style={{fontSize: '20px'}}> SHALU RANI</h1>
+
+               <div class="a"> <ul>
+                  <li>25 January 1993</li>
+                  <li>jadaunshalu@gmail.com</li>
+                  <li>+91 7895328523</li>
+                  <li>linkedin.com/in/shalu- singh-02310516a</li>
+               </ul>
+               </div>
+
+               <h2 style={{fontSize: '20px'}}>EDUCATION</h2>
+
+               <div class="b"> <ul>
+                  <li>MTech CSE VIT University,Vellore | 2019 | GPA:7.93/10</li>
+                  <li>BTech Computer Science Jayoti Vidyapeeth Women’s University | 2016| GPA:6.9/10</li>
+                     </ul> </div>
+
+                  <h3 style={{fontSize: '20px'}}>SKILLS</h3>
+
+                  <div class="c"> <ul>
+                     <li>Languages: C, C#, Java, DataStructure </li>
+                     <li>SQL: MSSQL SERVER, MYSQL</li>
+                     <li>NoSql: Mongodb</li>
+                        </ul></div>
+
+                     <h4 style={{fontSize: '20px'}}>AREA OF INTEREST</h4>
+
+                     <div class="d"> <ul>
+                        <li>Java, Asp.net, Database, Machine Learning</li>
+                          </ul> </div>
+
+                        <h5 style={{fontSize: '20px'}}>EXTRA-CURRICULAR</h5>
+                        <div class="e"> <ul>
+                           <li>Won 2nd price under radio mirchi 98.3 FM dancing concert at JVWU</li>
+                           <li>Member of Computer Society of India.</li>
+                           <li>Member of Charity and money raising event of “All Saints’ School.</li>
+                          </ul> </div>
+
+                           <h6 style={{fontSize: '20px'}}>Language:</h6>
+                           <div class="f"> <ul>
+                              <li>English</li>
+                              <li>Hindi</li>
+                             </ul> </div>
+               </div>
+
+                             
+                             
+                              <div class="aside">
+                             
+                              <h1 style={{fontSize: '20px'}}>INDUSTRY EXPERIENCE</h1>
+                              
+                              <div class ="g"> <ul>
+                                 <li>Since September’18Intern as a Software Engineer Period 8 Months	Lobotus Technology, Bengaluru</li>
+                                </ul> </div>
+                             
+                              <h2 style={{fontSize: '20px'}}>Experienced in Lobotus Technology:</h2>
+                             
+                              <div class ="h"> <ul> 
+                                 <li>Oct-Nov’18MSSql Server Stored procedures, Functions, automate curd operations and Three tier Architecture of Asp.net.
+                                     Technology Used: Asp.net, SqlServer.</li>
+                        <li>Nov-March’18Fetch Data from database and stored in    ExcelSheet and Automate for sending mail daily to the team leader,
+                                          Automate Notification for the user who forget to checkout from the Application,
+                                          Encryption and Decryption.
+                    Technology Used: Asp.net, SqlServer.
+                          </li>
+                              <li>March-may’19Pull C# project from Github and Build it in Jenkins,
+                                                                      Whenever code commit occur build should be triggered, 
+                                                                      SonarQube for continuous inspection of code quality.  
+                                                                    Technology Used: Jenkins, Git, SonarQube.
+                               </li>
+                              </ul> </div>
+
+
+                                    <h3 style={{fontSize: '20px'}}>PROJECT</h3>
+                                   
+                                    <div class ="i"> <ul> 
+                                       <li>Apr’16A Online Job Portal. </li>
+                                       <li>March’18Smart watch for coal miners using direct WIFI.</li>
+                                       <li>Feb’18Modified low power encryption algorithm (LEA) for fast encryption and   decryption.</li>
+                                       <li>jan’18Performance based enhancement of big data using Clustering algorithms.</li>
+                                      </ul> </div>
+
+                                       <h4 style={{fontSize: '20px'}}>ACADEMIC MAJOR RESEARCH PROJECT</h4>
+                                      
+                                      
+                                       <div class ="j"> <ul> 
+                                          <li>Sep-May’19Sentimental analysis using Naïve Bayes and SVM algorithms
+                                               Technology Used: Machine Learning, Python</li>
+                                          <li>May-July’18Application recommendation system using cosine similarity Algorithm
+                                                                            Technology Used: Machine Learning, Python, NoSql
+                                          </li>
+                                          </ul></div>
+
+                                           <h5 style={{fontSize: '20px'}}>ACHIEVEMENT AND RECOGNITIONS</h5>
+                                          
+                                           <div class ="k"> 
+                                           <ul>
+                                              <li>2013Participated in an event on “Ethical Hacking” of RoboFiesta in association with IIT Gandhi nagar.</li>
+                                              <li>2014Attended a National Conference on “Recent Advances in Electronics     &Computer Engineering (RAECE) conducted by IIT roorkee.</li>
+                                              <li>2015Training on Software development fundamentals by MICROSOFT.</li>
+                                             </ul> </div>
+
+                                             <h6 style={{fontSize: '20px'}}>APPLIED FOR PAPER PUBLICATION</h6>
+                                             <div class ="l"> <ul>
+                                                <li>Sentimental analysis using Naïve Bayes and SVM algorithms VIT VELORE, India</li>
+                                               </ul> </div>
+                                               </div>
+                
+        
+    </div>
+    </div>
+    
+                                             );
+                                          }
+                                       }
+export default App;
